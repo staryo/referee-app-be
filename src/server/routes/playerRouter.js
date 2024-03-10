ddf52406a53721dfb9b09ba39daddb8442b517de
elik <elik@example.com>
2024-03-10 15:08:51 +0300

    Add player Router and Service

---
+++ src/server/routes/playerRouter.js
@@ -0,0 +1,63 @@
+const Router = require("express")
+const router = new Router()
+const playerService = require("../services/playerService")
+
+const validateRequestMiddleware = require("../middleware/validateRequestMiddleware")
+const authMiddleware = require("../middleware/authMiddleware")
+const adminMiddleware = require("../middleware/adminMiddleware")
+
+const {
+  CREATE, UPDATE, DATA, DELETE, 
+} = require("./consts");
+
+// --- /api/player/create [POST] --- //
+router.post(CREATE,
+  authMiddleware,
+  validateRequestMiddleware({
+    type: "object",
+    properties: {
+      first_name: { type: "string",minLength: 2  },
+      last_name: { type: "string", minLength: 2 },
+      description: { type: "string" },
+      
+
+    },
+    required: ["first_name", "last_name", "description"],
+  }),
+  playerService.create,
+)
+
+// --- /api/player/update/:id [PUT] --- //
+router.put(`${UPDATE}/:id`,
+  authMiddleware,
+  validateRequestMiddleware({
+    type: "object",
+    properties: {
+        first_name: { type: "string",minLength: 2  },
+        last_name: { type: "string", minLength: 2 },
+        description: { type: "string" },
+        
+  
+      },
+    required: [],
+  }),
+  playerService.update,
+)
+
+
+
+// --- /api/player/data/:id [GET] --- //
+router.get(`${DATA}/:id`,
+  authMiddleware,
+  playerService.data,
+)
+
+// --- /api/player/delete/:id [DELETE] --- //
+router.delete(`${DELETE}/:id`,
+  authMiddleware,
+  adminMiddleware,
+  playerService.delete,
+)
+
+
+module.exports = router
---
+++ src/server/services/playerService.js
@@ -0,0 +1,93 @@
+const { db } = require("../../database/models");
+const tournament = require("../../database/models/tournament");
+const ApiError = require("../error/ApiError");
+const { Op } = require("sequelize");
+const Tournament = db.tournament
+const Player = db.player
+const Match = db.match
+const User = db.user
+
+class playerService {
+  async create(req, res, next) {
+    try {
+      const player_data = req.body
+      console.log(req.user)
+      const player_id = await Tournament.findOne({
+        where: {
+          id: req.tournament.id,
+        },
+      })
+      const player = await Player.create(player_data)
+      await player.addUser(player_id)
+
+      return res.json(player)
+    } catch (e) {
+      next(ApiError.internal(e.message))
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      const { id } = req.params
+    
+      const player_data = req.body
+      const  player= Player.findOne({
+        where: {
+            id,
+          },
+          include: {
+            Tournament,
+            where: {
+              id: req.tournament.id,
+            },
+          },
+       
+      });
+      console.log(player)
+    
+      return res.json(
+        await player.update(player_data),
+      )
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
+  }
+
+  async data(req, res, next) {
+    try {
+      const { id } = req.params
+      const player = await Player.findOne({
+        where: {
+          id,
+        },
+        include: {
+          Tournament,
+          where: {
+            id: req.tournament.id,
+          },
+        },
+      })
+      return res.json(player)
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
+  }
+
+
+
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params
+      return res.json(await Player.destroy({
+        where: {
+          id,
+        },
+      }))
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
+  }
+
+}
+
+module.exports = new playerService()
